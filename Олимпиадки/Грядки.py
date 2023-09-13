8bf8f81c169a6ca6dae2892784a27560d21a0384
Erdmond <kekizgorodyn@mail.ru>
2023-09-13 17:28:06 +0600

    -

--- Олимпиадки/Грядки.py
+++ Олимпиадки/Грядки.py
@@ -1,7 +1,12 @@
-k = 1
-l = 7
-n = 5
-m = 10
+# k = 1, k = 2, k = 3, k = 20
+# l = 7
+# n = 5
+# m = 10
+
+k = int(input())
+l = int(input())
+n = int(input())
+m = int(input())
 
 s1 = 2 * l * k + (2 * m + 2 * n) * k + (2 * m + m * (k - 2)) * (k - 1)
 print(s1)
@@ -11,8 +16,3 @@ while k > 0:
     s2 += 2 * l + 2 * m + 2*n + 2 * m * (k - 1)
     k = k - 1
 print(s2)
-
-# k = 1, k = 2, k = 3, k = 20
-# l = 7
-# n = 5
-# m = 10
