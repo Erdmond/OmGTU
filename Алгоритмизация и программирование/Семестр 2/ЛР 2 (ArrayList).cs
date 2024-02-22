3659693a4ff15cb7a76a169109ff51bd985e6497
Erdmond <kekizgorodyn@mail.ru>
2024-02-22 10:24:07 +0600

    Update ЛР 2 (ArrayList).cs

--- Алгоритмизация и программирование/Семестр 2/ЛР 2 (ArrayList).cs
+++ Алгоритмизация и программирование/Семестр 2/ЛР 2 (ArrayList).cs
@@ -16,252 +16,111 @@ using System.IO;
 using System.Security.Cryptography;
 using OmGTU;
 using System.Diagnostics;
-
+using System.Collections;
 namespace OmGTU
 {
     public class Menu
     {
-        public void Show_Menu(ref List<Classes> s, Classes r, ref bool t)
+        public static int Show_Menu()
         {
-            Console.Write("Меню:\n\t1) Создание базы данных;\n\t2) Добавление элементов в базу данных;\n\t3) Изменение базы;\n\t4) Выборка по кол-ву посадочных мест;\n\t5) Выборка по наличию проеткоров;\n\t6) Выборка по наличию ПК и кол-ву посадочных мест;\n\t7) Выборка по этажу;\n\t8) Вывод всей базы данных;\n\t9) Выход.\nДля выбора действия введите его номер: ");
-            int x = int.Parse(Console.ReadLine());
-            if (x == 1)
-            {
-                r.Create_Base(ref s, r);
-                Console.WriteLine("База успешно создана, производится возврат к меню.\n");
-            }
-            else if (x == 2)
-            {
-                r.Add_To_Base(ref s, r);
-                Console.WriteLine("Элемент успешно добавлен, производится возврат к меню.\n");
-            }
-            else if (x == 3)
-            {
-                r.Edit_Base(ref s, r);
-                Console.WriteLine("Элемент успешно отредактирован, производится возврат к меню.\n");
-            }
-            else if (x == 4)
-            {
-                r.Selection_By_Count(s, r);
-                Console.WriteLine("Выборка совершена, производится возврат к меню.\n");
-            }
-            else if (x == 5)
-            {
-                r.Selection_By_Proj(s, r);
-                Console.WriteLine("Выборка совершена, производится возврат к меню.\n");
-            }
-            else if (x == 6)
-            {
-                r.Selection_By_Pc_Count(s, r);
-                Console.WriteLine("Выборка совершена, производится возврат к меню.\n");
-            }
-            else if (x == 7)
-            {
-                r.Selection_By_Floor(s, r);
-                Console.WriteLine("Выборка совершена, производится возврат к меню.\n");
-            }
-            else if (x == 8)
-            {
-                r.All_Base(ref s);
-                Console.WriteLine("Информация выведена, производится возврат к меню.\n");
-            }
-            else if (x == 9)
-            {
-                Console.WriteLine("Программа завершена.");
-                t = false;
-            }
-            else
-            {
-                Console.WriteLine("Произошла ошибка.");
-            }
+            Console.WriteLine("\t\tМеню:");
+            Console.WriteLine("1 - Создать ArrayList");
+            Console.WriteLine("2 - Добавить элемент");           // Add()
+            Console.WriteLine("3 - Изменить элемент");           // ArrayList[x] = newLine
+            Console.WriteLine("4 - Удалить элемент");            // Remove()
+            Console.WriteLine("5 - Подсчитать кол-во элемента"); // Count
+            Console.WriteLine("6 - Бинарный поиск элемента");    // Sort()   //BinarySearch()
+            Console.WriteLine("7 - Копировать элементы");        // CopyTo()
+            Console.WriteLine("8 - Поиск индекса элемента");     // IndexOf()
+            Console.WriteLine("9 - Вставить элемент");           // Insert()
+            Console.WriteLine("10 - Развернуть порядок");        // Reverse()
+            Console.WriteLine("11 - Сортировать элементы");      // Sort()
+            Console.WriteLine("12 - Вывести ArrayList");
+            Console.WriteLine("13 - Выйти из программы");
+            Console.Write("\nВведите номер действия: ");
+            int result = int.Parse(Console.ReadLine());
+            return result;
         }
     }
-    public class Classes
-    {
-        public int Num { get; set; }
-        public int Count { get; set; }
-        public bool Proj { get; set; }
-        public bool Pc { get; set; }
-        public Classes(int n, int c, bool p, bool pc) { Num = n; Count = c; Proj = p; Pc = pc; }
-        public void Add_To_Base(ref List<Classes> x, Classes r)
-        {
-            bool f = false;
-            int n = 0;
-            int c = 0;
-            bool p = false;
-            bool pc = false;
-            do
-            {
-                try
-                {
-                    Console.Write("Введите номер кабинета трёхзначным числом: ");
-                    n = int.Parse(Console.ReadLine());
-                    if (n < 100 || n > 999) { Console.WriteLine("Ошибка формата данных. Попробуйте снова."); continue; }
-                    Console.Write("Введите кол-во посадочных мест: ");
-                    c = int.Parse(Console.ReadLine());
-                    Console.Write("Введите 1 если присутствует проектор, 0 если нет: ");
-                    p = Convert.ToBoolean(int.Parse(Console.ReadLine()));
-                    Console.Write("Введите 1 если присутствует ПК, 0 если нет: ");
-                    pc = Convert.ToBoolean(int.Parse(Console.ReadLine()));
-                    Console.WriteLine();
-                    f = true;
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("Ошибка формата данных. Попробуйте снова.");
-                    f = false;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Ошибка формата данных. Попробуйте снова.");
-                    f = false;
-                }
-            } while (!f);
-            Classes a = new Classes(n, c, p, pc);
-            x.Add(a);
-        }
-        public void Create_Base(ref List<Classes> x, Classes r)
-        {
-            Console.Write("Количество элементов базы: ");
-            int s = int.Parse(Console.ReadLine());
-            x = new List<Classes>();
-            for (int i = 0; i < s; i++)
-            {
-                r.Add_To_Base(ref x, r);
-            }
-        }
-        public void Edit_Base(ref List<Classes> x, Classes r)
-        {
-            Console.Write("Номер кабинета, который необходимо изменить: ");
-            int y = int.Parse(Console.ReadLine());
-            for (int i = 0; i < x.Count; i++)
-            {
-                bool q = false;
-                if (x[i].Num == y)
-                {
-                    q = true;
-                    bool f = false;
-                    do
-                    {
-                        try
-                        {
-                            Console.Write("Введите новый номер кабинета трёхзначным числом: ");
-                            int n = int.Parse(Console.ReadLine());
-                            if (n < 100 || n > 999) { Console.WriteLine("Ошибка формата данных. Попробуйте снова."); continue; }
-                            x[i].Num = n;
-                            Console.Write("Введите новое кол-во посадочных мест: ");
-                            x[i].Count = int.Parse(Console.ReadLine());
-                            Console.Write("Введите 1 если теперь присутствует проектор, 0 если нет: ");
-                            x[i].Proj = Convert.ToBoolean(int.Parse(Console.ReadLine()));
-                            Console.Write("Введите 1 если теперь присутствует ПК, 0 если нет: ");
-                            x[i].Pc = Convert.ToBoolean(int.Parse(Console.ReadLine()));
-                            Console.WriteLine();
-                            f = true;
-                        }
-                        catch (FormatException e)
-                        {
-                            Console.WriteLine("Ошибка формата данных. Попробуйте снова.");
-                            f = false;
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine("Ошибка формата данных. Попробуйте снова.");
-                            f = false;
-                        }
-                    } while (!f);
-                }
-                if (q == false) { Console.WriteLine("Не найдено таких кабинетов, возврат в меню."); }
-            }
-        }
-        public void Selection_By_Count(List<Classes> x, Classes r)
-        {
-            Console.Write("Кол-во посадочных мест: ");
-            int w = int.Parse(Console.ReadLine());
-            foreach (Classes i in x)
-            {
-                if (i.Count >= w)
-                {
-                    i.Info(i);
-                }
-            }
-        }
-        public void Selection_By_Proj(List<Classes> x, Classes r)
-        {
-            Console.Write("Наличие проектора (1 - есть, 0 - нет): ");
-            string o = Convert.ToString(Console.ReadLine());
-            bool re = false;
-            if (o == "1") { re = true; }
-            if (o == "0") { re = false; }
-            foreach (Classes i in x)
-            {
-                if (re)
-                {
-                    i.Info(i);
-                }
-            }
-        }
-        public void Selection_By_Pc_Count(List<Classes> x, Classes r)
-        {
-            Console.Write("Наличие ПК (1 - есть, 0 - нет): ");
-            string o = Convert.ToString(Console.ReadLine());
-            bool re = false;
-            if (o == "1") { re = true; }
-            if (o == "0") { re = false; }
-            foreach (Classes i in x)
-                Console.Write("Кол-во посадочных мест: ");
-            int v = int.Parse(Console.ReadLine());
-            foreach (Classes i in x)
-            {
-                if (re & i.Count >= v)
-                {
-                    i.Info(i);
-                }
-            }
-        }
-        public void Selection_By_Floor(List<Classes> x, Classes r)
-        {
-            Console.Write("Этаж: ");
-            Char v = Convert.ToChar(Console.ReadLine());
-            foreach (Classes i in x)
-            {
-                if (Convert.ToString(i.Num)[0] == v)
-                {
-                    i.Info(i);
-                }
-            }
-        }
-        public void All_Base(ref List<Classes> x)
-        {
-            foreach (Classes c in x)
-            {
-                c.Info(c);
-            }
-        }
-        public void Info(Classes c)
-        {
-            Console.WriteLine("---");
-            Console.WriteLine($"Номер кабинета: {c.Num};");
-            Console.WriteLine($"Кол-во посадочных мест: {c.Count};");
-            if (c.Proj == true) { Console.WriteLine("В кабинете есть проектор;"); }
-            else { Console.WriteLine("В кабинете нет проектора;"); }
-            if (c.Pc == true) { Console.WriteLine("В кабинете есть компьютеры.;"); }
-            else { Console.WriteLine("В кабинете нет компьютеров.;"); }
-        }
-    }
-    internal class First
+    internal class Program
     {
         public static void Main()
         {
-            bool u = true;
-            Classes r = new Classes(0, 0, false, false);
-            List<Classes> Cabinet = new List<Classes>();
-            Menu menu = new Menu();
-            while (u)
+            int i = 0;
+            ArrayList Collection = new ArrayList();
+            while (true)
             {
-                menu.Show_Menu(ref Cabinet, r, ref u);
+                switch (Menu.Show_Menu())
+                {
+                    case 1:
+                        Collection = new ArrayList();
+                        Console.Write("Введите количество элементов, которое необходимо внести в лист: ");
+                        i = int.Parse(Console.ReadLine());
+                        for (int j = 0; j < i; j++) { Collection.Add(Console.ReadLine()); }
+                        break;
+                    case 2:
+                        Console.Write("Введите элемент воторый необходимо добавить: ");
+                        Collection.Add(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.Write("Введите номер элемента который необходимо заменить: ");
+                        i = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите новый элемент: ");
+                        Collection[i] = Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Write("Введите индекс элемента который необохдимо удалить: ");
+                        i = int.Parse(Console.ReadLine());
+                        Collection.Remove(i);
+                        break;
+                    case 5:
+                        i = Collection.Count;
+                        Console.WriteLine($"Кличество элементов в листе: {i}");
+                        break;
+                    case 6:
+                        Collection.Sort();
+                        Console.Write("Введите элемент который необходимо найти: ");
+                        var r = Console.ReadLine();
+                        Collection.BinarySearch(r);
+                        break;
+                    case 7:
+                        Console.Write("Введите с индекс с которого нужно скопировать элементы: ");
+                        i = int.Parse(Console.ReadLine());
+                        Console.Write("Введите с индекс по который нужно скопировать элементы: ");
+                        int e = int.Parse(Console.ReadLine());
+                        int[] CopyArray = new int[e - i];
+                        Collection.CopyTo(i, CopyArray, 0, e - i);
+                        break;
+                    case 8:
+                        Console.Write("Введите элемент индекс которого необходимо найти: ");
+                        r = Console.ReadLine();
+                        Console.WriteLine($"Индекс элемента {r}: {Collection.IndexOf(r)}");
+                        break;
+                    case 9:
+                        Console.Write("Введите элемент который необходимо вставить: ");
+                        r = Console.ReadLine();
+                        Console.WriteLine("Введите индекс нового элемента: ");
+                        i = int.Parse(Console.ReadLine());
+                        Collection.Insert(i, r);
+                        break;
+                    case 10:
+                        Collection.Reverse();
+                        break;
+                    case 11:
+                        Collection.Sort();
+                        break;
+                    case 12:
+                        foreach (var item in Collection) { Console.WriteLine(item); }
+                        break;
+                    case 13:
+                        Environment.Exit(0);
+                        break;
+                }
             }
         }
     }
 }
-/* 2 класса: 1 - класс меню (создание базы, добавление элементов базы, модификация эл-в по номеру кабинета, выборка каб-в с кол-м посадочных мест >= заданному, выборка кабинетов с проектором, выборка кабинетов с компьютерами и кол-вом посадочных мест >= заданному, вывод всей информации базы, выборка кабнетов на одном этаже, выход);
- *           2 - класс кабинетов (номер, кол-во посадочных мест, наличие проектора, наличие ПК); предусмотреть после пункта меню возврат в основное, предусмотреть проверку данных (если база не заполненна, то выборки делать нельзя)*/
+/*
+Array:       Count, BinSearch, Copy, Find, FindLast, IndexOf, Reverse, Resize, Sort
+SortedList:  Add, IndexOf и по ключу и по значению, Вывод ключа по индексу, Вывод значения по индексу
+*/
